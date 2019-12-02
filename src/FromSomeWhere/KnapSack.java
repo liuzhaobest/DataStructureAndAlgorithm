ae718fcbc1e8dedd40b34ebbc9eaa0d292849d75
zhaoliu <liuzhaobest@gmail.com>
2019-12-02 15:50:05 -0800

    done with the unbounded one for knapSack. Is there a better solution?

--- src/FromSomeWhere/KnapSack.java
+++ src/FromSomeWhere/KnapSack.java
@@ -1,13 +1,57 @@
 package FromSomeWhere;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class KnapSack {
     public static void main(String[] args) {
-        int weightLimit = 15;
-        int[] weightArr = {1,2,8,4,5};
-        int[] valueArr = {2,3,17,5,6};
-        System.out.println(solution(weightArr,valueArr,weightLimit));
+        int weightLimit = 7;
+        int[] weightArr = {2,3};
+        int[] valueArr = {3,7};
+        System.out.println(solutionUnbounded(weightArr,valueArr,weightLimit));
     }
+
+    public static int solutionUnbounded(int[] weightArr, int[] valueArr, int weightLimit){
+        if(weightArr == null || valueArr == null || valueArr.length == 0 || weightArr.length == 0 || weightLimit == 0){
+            return 0;
+        }
+        int[][] dp = new int[weightArr.length][weightLimit + 1];
+        for (int i = 0; i < weightLimit+1; i++) {
+            dp[0][i] = (i / weightArr[0] ) * valueArr[0];
+            //System.out.print(dp[0][i] + " ");
+
+        }
+        //System.out.println(" ");
+        for (int i = 1; i < weightArr.length; i++) {
+            for (int j = 0; j < weightLimit+1; j++) {
+                if(weightArr[i] > j){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = unboundedHelper(dp,i,j,weightArr,valueArr);
+                }
+                //System.out.print(dp[i][j] + " ");
+            }
+        }
+        return dp[weightArr.length - 1][weightLimit];
+    }
+
+    public static int unboundedHelper(int[][] dp, int curItem, int curLimit, int[] weightArr, int[] valueArr){
+        // i need to have the maximum number among 1. 0 to curLimit / weightArr[curItem] number of curItem
+        int length = curLimit / weightArr[curItem] + 1;
+        Integer[] result = new Integer[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = dp[curItem - 1][curLimit - weightArr[curItem] * i] + valueArr[curItem] * i;
+        }
+        return Collections.max(Arrays.asList(result));
+
+    }
+
+
     public static int solution(int[] weightArr, int[] valueArr, int weightLimit){
+        if(weightArr == null || valueArr == null || valueArr.length == 0 || weightArr.length == 0 || weightLimit == 0){
+            return 0;
+        }
         int[][] dp = new int[weightArr.length][weightLimit + 1];
         // initialize the first line
         for (int i = 0; i < weightLimit+1; i++) {
